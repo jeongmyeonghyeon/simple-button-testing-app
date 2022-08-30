0df6f1343372b6baa6e93b59c6ae9d7b7d75766a
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-30 14:02:36 +0900

    증가/감소 테스트 코드 및 기능 구현

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,8 @@
-import { render, screen } from "@testing-library/react";
+/**
+ * fireEvent - 유저가 발생시키는 액션(이벤트)에 대한 테스트를 해야 하는 경우 사용.
+ * (https://testing-library.com/docs/dom-testing-library/api-events)
+ */
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("the counters starts at 0", () => {
@@ -21,4 +25,24 @@ test("plus button has correct text", () => {
   render(<App />);
   const buttonElement = screen.getByTestId("plus-button");
   expect(buttonElement).toHaveTextContent("+");
+});
+
+test("when the plus button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  // click plus-button
+  fireEvent.click(buttonElement);
+
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test("when the minus button is pressed, the counter changes to -1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  // click minus-button
+  fireEvent.click(buttonElement);
+
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
 });
