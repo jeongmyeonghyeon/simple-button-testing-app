a0d9d66759a3c11c06c874dbb43aeb7117fa8538
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-30 13:45:23 +0900

    카운터 초기값(0) 테스트 코드 구현

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counters starts at 0", () => {
+  // App 컴포넌트를 render 함.
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해서 원하는 Element 접근(접근할 때 id로 접근)
+  const counterElement = screen.getByTestId("counter");
+  // id가 counter인 Element 텍스트가 0인지 테스트
+  // expect(counterElement).toBe(0); // error
+  expect(counterElement).toHaveTextContent(0);
 });
